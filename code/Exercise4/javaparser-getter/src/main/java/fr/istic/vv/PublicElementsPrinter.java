cca5444ce5b38b33f8fcdb0354b115c5a4ad80c7
tpicaud <tom.picaud@etudiant.univ-rennes1.fr>
2022-12-02 11:01:00 +0100

    update exercise 4

--- code/Exercise4/javaparser-getter/src/main/java/fr/istic/vv/PublicElementsPrinter.java
+++ code/Exercise4/javaparser-getter/src/main/java/fr/istic/vv/PublicElementsPrinter.java
@@ -17,9 +17,9 @@ import com.github.javaparser.ast.visitor.VoidVisitorWithDefaults;
 // prints all public enum, classes or interfaces along with their public methods
 public class PublicElementsPrinter extends VoidVisitorWithDefaults<Void> {
 
-    List<String> var = new ArrayList<String>();
-    List<String> method = new ArrayList<String>();
-    List<String> no_getter = new ArrayList<String>();
+    List<VariableDeclarator> varPrivate = new ArrayList<VariableDeclarator>();
+    private List<String> methodPublic = new ArrayList<String>();
+    private List<String> no_getter = new ArrayList<String>();
 
     @Override
     public void visit(CompilationUnit unit, Void arg) {
@@ -29,10 +29,13 @@ public class PublicElementsPrinter extends VoidVisitorWithDefaults<Void> {
     }
 
     public void visitTypeDeclaration(TypeDeclaration<?> declaration, Void arg) {
-        if (!declaration.isPublic())
-            return;
-        System.out.println(declaration.getFullyQualifiedName().orElse("[Anonymous]"));
+        String class_name = declaration.getNameAsString();
+        String package_name = declaration.findCompilationUnit().get().getPackageDeclaration().get().getNameAsString();
+
         for (MethodDeclaration method : declaration.getMethods()) {
+            if (declaration.isPublic()) {
+                methodPublic.add(declaration.getNameAsString().toLowerCase());
+            }
             method.accept(this, arg);
         }
         // Printing nested types in the top level
@@ -40,9 +43,14 @@ public class PublicElementsPrinter extends VoidVisitorWithDefaults<Void> {
             if (member instanceof TypeDeclaration)
                 member.accept(this, arg);
         }
-        for (FieldDeclaration method : declaration.getFields()) {
-            method.accept(this, arg);
+        for (FieldDeclaration field : declaration.getFields()) {
+            if (field.isPrivate()) {
+                varPrivate.add(field.getVariable(0));
+            }
+            field.accept(this, arg);
         }
+
+        fillNoGetter(package_name,class_name);
     }
 
     @Override
@@ -51,52 +59,47 @@ public class PublicElementsPrinter extends VoidVisitorWithDefaults<Void> {
     }
 
     @Override
-    public void visit(EnumDeclaration declaration, Void arg) {
-        // var.add(declaration.toString());
-        // visitTypeDeclaration(declaration, arg);
-    }
-
-    @Override
     public void visit(MethodDeclaration declaration, Void arg) {
-        method.add(declaration.getNameAsString().toLowerCase());
-
-        // if(!declaration.isPublic()) return;
-        // System.out.println(" " + declaration.getDeclarationAsString(true, true));
+        // if (declaration.isPrivate()) {
+        //     methodPublic.add(declaration.getNameAsString().toLowerCase());
+        // }
+        //visitTypeDeclaration(declaration, arg);
     }
 
     @Override
     public void visit(FieldDeclaration declaration, Void arg) {
-        for (VariableDeclarator variable : declaration.getVariables()) {
-            var.add(variable.getNameAsString());
-        }
-        // if(!declaration.isPublic()) return;
-        // System.out.println(" " + declaration.getDeclarationAsString(true, true));
+        // if (declaration.isPrivate()) {
+        //     for (VariableDeclarator variable : declaration.getVariables()) {
+        //         varPrivate.add(variable);
+        //     }
+        // }
+        
     }
 
-    public List<String> getNoGetter() {
-        System.out.println(var);
-        System.out.println(method);
-
-        for (String var_name : var) {
-            if (!method.contains("get" + var_name))
-                no_getter.add(var_name);
+    public void fillNoGetter(String package_name, String class_name) {
+        for (VariableDeclarator var : varPrivate) {
+            if (!methodPublic.contains("get" + var.getNameAsString()))
+                no_getter.add("name: "+var.getNameAsString()+"; class: "+class_name+"; package: "+package_name);
         }
-        return no_getter;
     }
 
     public void createFile() throws FileNotFoundException, UnsupportedEncodingException {
         List<String> to_write = getNoGetter();
 
         PrintWriter writer = new PrintWriter("no_getter.txt", "UTF-8");
-        writer.println("Arrtibuts sans getter :\n");
+        writer.println("Private field without getter :\n");
         for (String line : to_write) {
             writer.println(line);
         }
         writer.close();
     }
 
-    public List<String> getVar() {
-        return var;
+    public List<VariableDeclarator> getVar() {
+        return varPrivate;
+    }
+
+    public List<String> getNoGetter() {
+        return no_getter;
     }
 
 }
